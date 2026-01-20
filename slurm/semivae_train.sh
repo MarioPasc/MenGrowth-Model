80e5f60f53958bcfb35852bb113151c460922785
MPascual <mario.pg02@gmail.com>
2026-01-20 09:54:13 +0100

    Modullate semivae resources

--- slurm/semivae_train.sh
+++ slurm/semivae_train.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -J log_semivae
-#SBATCH --time=5-00:00:00
+#SBATCH --time=1-12:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --constraint=dgx
 #SBATCH --gres=gpu:2
